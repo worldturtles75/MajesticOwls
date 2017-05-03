23bdb55340078a7653b966bdcb17c249a34bfee1
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-03 11:58:56 -0700

    ADD DATABASE

---
+++ database/index.js
@@ -0,0 +1,26 @@
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/greenfield');
+
+var db = mongoose.connection;
+
+db.on('error', function() {
+  console.log('mongoose connection error');
+});
+
+db.once('open', function() {
+  console.log('mongoose connected successfully');
+})
+
+
+var userSchema = mongoose.Schema({
+
+    user: {type: String, unique: true}
+
+  })
+
+
+var user = mongoose.moddel('user', userSchema);
+
+
+export.modules = user;
