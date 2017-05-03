873f2dcbe9f0fd185f36a4d6eed55fe9a1df5bef
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-02 20:20:07 -0700

    REACT PACKAGE BUILD OUT

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,10 @@ var config = {
     {
       test : /\.jsx?/,
       include : APP_DIR,
-      loader : 'babel-loader'
+      loader : 'babel-loader',
+      query: {
+          presets: ['react', 'es2015']
+       }
     }
   ]
 }
