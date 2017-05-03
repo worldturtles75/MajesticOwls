38475935046597d9a5b6a3060e022b694d9e0a37
Jim Lee <jimleeisme@gmail.com>
2017-05-03 11:25:25 -0700

    (fix) Fixed webpack filepath

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'react-client/dis');
+var BUILD_DIR = path.resolve(__dirname, 'react-client/dist');
 var APP_DIR = path.resolve(__dirname, 'react-client/src');
 
 var config = {
