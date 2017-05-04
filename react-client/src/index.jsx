72a812a132915dc719f9427fe51cbb85ecfb619d
Anthony Wong <shrimpwongton@users.noreply.github.com>
2017-05-03 20:20:11 -0700

    Got helper functions set up

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -6,12 +6,30 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.searchFlight = this.searchFlight.bind(this);
+    this.searchAddress = this.searchAddress.bind(this);
+    this.dropDownSelect = this.dropDownSelect.bind(this);
     this.state = { 
+      trips: [],
     }
   }
+
+  searchFlight (flight) {
+
+  }
+
+  searchAddress (address) {
+
+  }
+
+  dropDownSelect (trip) {
+
+  }
+
+
   render () {
     return ( 
-      <div>Hello Majestic Owls</div>
+      <div>Hello Majestic Owls, this is still in testing phases</div>
     )
   }
 }
