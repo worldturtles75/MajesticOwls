b646eb0dba9a4c49b77c0e533a3d84043c3438ec
Anthony <shrimpwongton@users.noreply.github.com>
2017-05-04 14:32:37 -0700

    Added Google Sign-In Button (#15)
    * Added google login button
    
    * Removed functions for now
    
    * Added ReactRouter, and sign-in button
    
    * Centered Sign-In Button
    
    * Replaced appBar with toolBar

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,37 +1,67 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Search from './components/Search.jsx';
+import SignIn from './components/SignIn.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.searchFlight = this.searchFlight.bind(this);
-    this.searchAddress = this.searchAddress.bind(this);
-    this.dropDownSelect = this.dropDownSelect.bind(this);
-    this.state = { 
-      trips: [],
-    }
-  }
-
-  searchFlight (flight) {
-
-  }
-
-  searchAddress (address) {
-
-  }
-
-  dropDownSelect (trip) {
-
-  }
-
-
-  render () {
-    return ( 
-      <div>Hello Majestic Owls, this is still in testing phases</div>
-    )
+const styles = {
+  titleStyle: {
+    top: 'auto',
+    right: 'auto',
+    left: 50,
+    bottom: 'auto',
+    position: 'fixed',
+  },
+  signInStyle: {
+    top:15,
+    right: 30,
+    left:'auto',
+    bottom: 'auto',
+    position:'fixed',
   }
 }
+
+const Home = () => (
+  <div>
+    <div> 
+      <MuiThemeProvider>
+        <Toolbar>
+          <ToolbarGroup firstChild={true} style={styles.titleStyle}>
+            <ToolbarTitle text="Majestic Owls" />
+          </ToolbarGroup>
+          <ToolbarGroup style={styles.signInStyle}>
+            <Link to='/sign-in'>
+              <FlatButton 
+                label="Sign In"
+              />
+            </Link>
+          </ToolbarGroup>
+        </Toolbar>
+      </MuiThemeProvider>
+    </div>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>Sign-In</h2>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/sign-in" component={SignIn}/>
+    </div>
+  </Router>
+)
 
 render(<App/>, document.getElementById('app'));
