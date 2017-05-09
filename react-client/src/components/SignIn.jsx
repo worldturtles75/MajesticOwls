76c3d3febf111f183411b5f940b3575d25979a21
Anthony Wong <shrimpwongton@users.noreply.github.com>
2017-05-09 15:16:12 -0700

    Centered Google Signin button

--- react-client/src/components/SignIn.jsx
+++ react-client/src/components/SignIn.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import GoogleButton from 'react-google-button'
 import {
   BrowserRouter as Router,
   Route,
@@ -15,7 +16,13 @@ const styles = {
     right: 'auto',
     left: 50,
     bottom: 'auto',
-    position: 'fixed',
+    position: 'relative',
+  },
+  signInStyle: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
   },
 }
 
@@ -28,6 +35,12 @@ const SignIn = () => (
         </ToolbarGroup>
       </Toolbar>
     </MuiThemeProvider>
+    <div
+      style = {styles.signInStyle} >
+      <GoogleButton 
+        onClick={() => { console.log('button clicked') }} 
+      />
+    </div>
   </div>
 )
 
