54d9b7c249fee3eea6c75503ee4abc9431ed38f3
Jim Lee <jimleeisme@gmail.com>
2017-05-10 18:11:08 -0700

    (fix) Put routes in a div

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SignIn from './components/SignIn.jsx';
 import DashBoard from './components/DashBoard.jsx';
-
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FlatButton from 'material-ui/FlatButton';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
@@ -65,9 +63,11 @@ class App extends React.Component {
 
     return (
       <Router>
-        <Route exact path="/" component={Home}/>
-        <Route path="/sign-in" component={SignIn}/>
-        <Route path="/dashboard" component={DashBoard}/>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/sign-in" component={SignIn}/>
+          <Route path="/dashboard" component={DashBoard}/>
+        </div>
       </Router>
     )
   }
