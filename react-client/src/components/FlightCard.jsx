e8f92d1580b7eff199f56095bf004064e3adf03c
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-11 15:56:46 -0700

    API Works for Flight card

--- react-client/src/components/FlightCard.jsx
+++ react-client/src/components/FlightCard.jsx
@@ -11,7 +11,23 @@ import StarBorder from 'material-ui/svg-icons/toggle/star-border';
  class FlightCard extends React.Component {
   constructor (props) {
     super(props);
+}
+
+  componentWillMount() {
+    this.flightSearch();
   }
+  flightSearch(airline,flight,month,day,year) {
+    return $.getJSON('https://crossorigin.me/https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/AA/102/arr/2017/5/11?appId=a187a7bc&appKey=e276d93bc5207238c1eacdd21bec0653&utc=false')
+        .then((data) => {
+          console.log('data',data);
+          this.setState({
+              departure: data.appendix.airports[0].city
+
+
+          });
+        });
+
+      }
 
   render() {
     const styles = {
@@ -33,6 +49,7 @@ import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 
           <Card style={styles.card}>
                <Subheader>Flight Info</Subheader>
+
 
                <ul>{this.props.flight.departurePort} | {this.props.flight.departureCity}  TO {this.props.flight.arrivalPort} | {this.props.flight.arrivalCity}</ul>
                <ul>{this.props.flight.leaveTime}</ul>
