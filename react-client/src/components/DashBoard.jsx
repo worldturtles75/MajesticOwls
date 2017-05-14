4b5e5559aadaa0851ff710e30307edae36188544
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-13 17:59:42 -0700

    small edits

--- react-client/src/components/DashBoard.jsx
+++ react-client/src/components/DashBoard.jsx
@@ -72,7 +72,6 @@ class DashBoard extends React.Component {
   }
 
   flightSearch(airline,flight,month,day,year) {
-    console.log('https://crossorigin.me/https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/'+airline+'/'+flight+'/arr/'+year+'/'+day+'/'+month+'?appId=' + require('../config/config').FLIGHTSTATUS.API_KEY + '&appKey=' + require('../config/config').FLIGHTSTATUS.APP_KEY + '&utc=false');
     return $.getJSON('https://crossorigin.me/https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/'+airline+'/'+flight+'/arr/'+year+'/'+day+'/'+month+'?appId=' + require('../config/config').FLIGHTSTATUS.API_KEY + '&appKey=' + require('../config/config').FLIGHTSTATUS.APP_KEY + '&utc=false')
         .then((data) => {
           console.log('data',data);
