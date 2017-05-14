2dd006bd98189738de85ea6e2435bfef04447a82
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-14 11:12:03 -0700

    Staging for rebase

--- server/index.js
+++ server/index.js
@@ -204,7 +204,7 @@ app.post('/database/save', (req,res) => {
         console.log('did not save');
       } else {
         console.log('history saved', result);
-      }
+        }
     })
     res.end();
 });
