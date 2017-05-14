ecb69c587cd2867dfe58bb80e40a836910f54079
Jim Lee <jimleeisme@gmail.com>
2017-05-13 17:25:52 -0700

    (fix) Add Google API key to config file

--- server/config.example.js
+++ server/config.example.js
@@ -1,6 +1,7 @@
 // Copy this file, rename to 'config.js', and paste in the values
-  module.exports = {
-    G_ID: 'CLIENT_ID_GOES_HERE',
-    G_SECRET: 'CLIENT_SECRET_GOES_HERE',
-    SESSION_SECRET: 'SESSION_SECRET_GOES_HERE'
-  };
+module.exports = {
+  G_ID: 'CLIENT_ID_GOES_HERE',
+  G_SECRET: 'CLIENT_SECRET_GOES_HERE',
+  SESSION_SECRET: 'SESSION_SECRET_GOES_HERE',
+  GOOGLE_KEY: 'GOOGLE_API_KEY_GOES_HERE'
+};
