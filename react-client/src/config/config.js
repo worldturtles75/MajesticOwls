ac544307cb05e211ef66a3f85fa216f9f046651b
Anthony <shrimpwongton@users.noreply.github.com>
2017-05-16 11:01:29 -0700

    Delete config.js

--- react-client/src/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports.YELP_TOKEN = 'TOKEN_GOES_HERE';
-module.exports.GOOGLE_KEY = 'AIzaSyCmUT8Iu75X-CW9BrHhXz9lnwvd2w4kzFI';
-module.exports.FLIGHTSTATUS = {
-  APP_KEY: 'e276d93bc5207238c1eacdd21bec0653',
-  API_KEY: 'a187a7bc'
-};
-
